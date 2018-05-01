2ac1c6b09dfe616d3df59cbfd628a0c62d4a2a8b
Eric Razor <eric.razorjr@gmail.com>
2018-05-01 06:09:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     new_v = programmer_hash.merge(" ")
+     new_v = [:programmer_hash][:known_for] == alans_new_info
      return new_v
      
      
